617dadf2aaa4e503dd622dfe724b865422f7d22f
BrayamFonck <brayamfonck@gmail.com>
2024-04-23 08:10:30 -0500

    Se agrega la segmentación media

--- Algorithm/img_processing.py
+++ Algorithm/img_processing.py
@@ -30,4 +30,24 @@ class BackgroundRemover:
         plt.axis('off')
         plt.show()
 
+  
+    def media(self, k):
+        # f es la imagen y k el tamaño del kernel
+        # f puede ser una imagen a color 
+        # Tamaño del vecindario para la desviación estándar local
+        nn = 15  
+        # Convertir a escala de grises
+        g1 = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)  
+        # Suavizado con filtro gaussiano
+        g1_std = cv2.GaussianBlur(g1, (nn, nn), nn/6, borderType=cv2.BORDER_REFLECT)  
+        # Gradiente en dirección x
+        g1_gradient = np.abs(cv2.Sobel(g1, cv2.CV_64F, 1, 0, ksize=3))  
+
+        # Combinar los descriptores en un solo mapa de descriptores
+        g = np.stack((g1_std, g1_gradient), axis=-1) 
+
+        # Reshape para que g sea del tamaño de f
+        g = g.reshape(-1, g.shape[2])
+
+
     # Lets add here some segmentation techniques
