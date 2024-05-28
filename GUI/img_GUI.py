55f5232e195383d1123fb230dc0cdfc4b4c44659
DANI15CN <live-daniela@hotmail.com>
2024-05-28 16:04:06 -0500

    lines 21- 23 and lines 36-51

--- GUI/img_GUI.py
+++ GUI/img_GUI.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QMessageBox, QPushButton, QFileDialog
 from PyQt5.QtGui import QPixmap
 
 class ImageEditorApp(QMainWindow):
@@ -18,6 +18,10 @@ class ImageEditorApp(QMainWindow):
         btn_load_image.setGeometry(50, 10, 100, 30)
         btn_load_image.clicked.connect(self.load_image)
 
+        btn_save_image = QPushButton("Guardar Imagen", self)
+        btn_save_image.setGeometry(160, 10, 100, 30)
+        btn_save_image.clicked.connect(self.save_image)
+
     def load_image(self):
         file_dialog = QFileDialog(self)
         file_dialog.setNameFilter("Archivos de Imagen (*.jpg *.jpeg *.png)")
@@ -28,3 +32,21 @@ class ImageEditorApp(QMainWindow):
             file_name = file_dialog.selectedFiles()[0]
             pixmap = QPixmap(file_name)
             self.label_image.setPixmap(pixmap)
+
+    def save_image(self):
+        pixmap = self.label_image.pixmap()
+        if pixmap:
+            save_dialog = QFileDialog(self)
+            save_dialog.setAcceptMode(QFileDialog.AcceptSave)
+            save_dialog.setNameFilter("Archivos de Imagen (*.jpg *.jpeg *.png)")
+            save_dialog.setDefaultSuffix('png')
+
+            if save_dialog.exec_():
+                save_path = save_dialog.selectedFiles()[0]
+                if pixmap.save(save_path):
+                    QMessageBox.information(self, "Guardar Imagen", "Imagen guardada exitosamente.")
+                else:
+                    QMessageBox.warning(self, "Guardar Imagen", "Error al guardar la imagen.")
+        else:
+            QMessageBox.warning(self, "Guardar Imagen", "No hay imagen para guardar.")
+    
