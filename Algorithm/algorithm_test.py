116f4e891c68d868089b0ab7bb03241766eb8a42
BrayamFonck <brayamfonck@gmail.com>
2024-06-27 20:59:30 -0500

    Show mask in algorithm_test in blank and white

--- Algorithm/algorithm_test.py
+++ Algorithm/algorithm_test.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-
 def main():
     # Image route
     image = 'img19_retrato.jpeg'
@@ -15,16 +14,26 @@ def main():
     # Use example
     bg_remover = BackgroundRemover()
     bg_remover.load_image(img_route)
-    bg_remover.show_image(bg_remover.image_rgb)
-    mask_list = {"get_semantic_segmentation": True,
-                 "get_ORB_segmentation":False}
+    # bg_remover.show_image(bg_remover.image_rgb)
+    mask_list = {"get_semantic_segmentation": True }
     bg_remover.get_final_mask(mask_dict=mask_list)
-    plt.imshow(bg_remover.get_semantic_segmentation(), "gray")
+    
+    # Assuming get_final_mask updates an attribute with the mask, e.g., bg_remover.segmentation_mask
     bg_remover.apply_final_mask()
     bg_remover.show_image(bg_remover.modified_image)
+    
+    # Show the segmentation mask
+    # This assumes there's a method show_image in BackgroundRemover and an attribute for the mask
+    bg_remover.show_image(bg_remover.get_semantic_segmentation())
+    # Obtener la máscara de segmentación semántica
+    semantic_mask = bg_remover.get_semantic_segmentation()
+    
+    # Graficar la máscara de segmentación semántica
+    plt.imshow(semantic_mask, cmap='gray')  # Asumiendo que es una imagen en escala de grises
+    plt.axis('off')  # Omitir los ejes para una visualización más limpia
+    plt.show()
 
     return 0
-
 
 if __name__ == '__main__':
     main()
